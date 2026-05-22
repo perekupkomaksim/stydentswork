c71904d8c02f56bc3ec52c59de8b2261ab95207c
stydentswork <perekupkomaksim8@gmail.com>
2026-05-22 08:56:47 +0300

    Лабораторна робота №22

--- ООП/LB22/WpfApp1/obj/Debug/net10.0-windows/WpfApp1.AssemblyInfo.cs
+++ ООП/LB22/WpfApp1/obj/Debug/net10.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3403e9ded46cd1b2fecf112bf13388c1bd29931")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
